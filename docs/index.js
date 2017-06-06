f46d2be18ddead5ce599379c1dd5c62c82048b24
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2017-06-05 21:41:14 -0400

    does this work?

---
+++ docs/index.js
@@ -0,0 +1,6 @@
+var GlitchImage = require('../');
+
+var img = new GlitchImage('https://www.instagram.com/p/BEm1yKquZqk/media/?size=m')
+
+var div = document.getElementById('main');
+div.appendChild(img);
